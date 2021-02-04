8ef2087a36d38d69482d80a2e11fc1024a1f5c29
OLIVIA LANG <owl5022@aci-lgn-005.acib.production.int.aci.ics.psu.edu>
2021-02-04 18:45:39 -0500

    minor fix in name of synthetic genome FASTA
    For consistent formatting, the name of the generated Reb1 locus inserted synthetic genome filename is fixed in this commit. Swap underscore for hyphen for name format consistency.

--- paper/SyntheticEpitope/job/generate_synthetic_genomes.sh
+++ paper/SyntheticEpitope/job/generate_synthetic_genomes.sh
@@ -8,7 +8,7 @@ RTAG=../input/RAND_500.fa
 [ -d synthetic_genome ] || mkdir synthetic_genome
 
 perl $INSERT ../input/sacCer3.fa chr14:241688:+ $RTAG synthetic_genome/sacCer3_Rap1-Nterm_R500.fa
-perl $INSERT ../input/sacCer3.fa chr2:334385:- $RTAG synthetic_genome/sacCer3_Reb1_Cterm_R500.fa
+perl $INSERT ../input/sacCer3.fa chr2:334385:- $RTAG synthetic_genome/sacCer3_Reb1-Cterm_R500.fa
 
 perl $INSERT ../input/hg19.fa chr16:67650679:+ $RTAG synthetic_genome/hg19_CTCF-Nterm_R500.fa
 perl $INSERT ../input/hg19.fa chr3:184079502:+ $RTAG synthetic_genome/hg19_POLR2H-Nterm_R500.fa
