01ea5b1d1bda05119db33f763747168bc3bf0d54
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2022-05-08 21:19:28 -0400

    update docusaurus Homepage
    The homepage is updated in this commit to match the default initialization format currently set up on the master branch. CSS and homepage (index.js) files are broken up to be more modular and this commit updates the broken up files to match the content of the old docusaurus version.

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -3,92 +3,38 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import styles from './index.module.css';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-const features = [
-  {
-    title: 'EpitopeID',
-    imageUrl: 'genopipe-img/epitopeid-icon.png',
-    description: (
-      <>
-        Check your FASTQ files to confirm epitope sequence insertions in genomic DNA.
-      </>
-    ),
-  },
-  {
-    title: 'DeletionID',
-    imageUrl: 'genopipe-img/deletionid-icon.png',
-    description: (
-      <>
-        Check your BAM files for regions with significant depletions of reads.
-      </>
-    ),
-  },
-  {
-    title: 'StrainID',
-    imageUrl: 'genopipe-img/strainid-icon.png',
-    description: (
-      <>
-        Check your BAM files for variants and see which strain your sample most closely resembles.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
   return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/welcome">
+            Get Started
+          </Link>
         </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
+      </div>
+    </header>
   );
 }
 
-function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
+      <HomepageHeader />
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+        <HomepageFeatures />
       </main>
     </Layout>
   );
 }
-
-export default Home;
