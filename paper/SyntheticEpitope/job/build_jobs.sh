2525aa629d6c04da869fab3eba48f7b775c91567
owlang <oliviawlang@gmail.com>
2022-08-03 08:11:48 -0400

    autobuild depth simulation submission scripts
    Dramatic restructure of simulations:
    
    `depth_simulations.txt` -- describes experimental designs
    *sequencing "depth" removed as a variable (implied run of each sequencing depth for every job)
    *synthetic epitope length added as a variable tested in each row/"experimental design"
    `job/build_jobs.sh` -- script to build PBS submission scripts based on the `depth_template.pbs` submission script using each "experimental design" row in `depth_simulations.txt`
    `job/depth_template.pbs` -- The outline for new simulation script that simulates a dataset at every depth tested.
    
    ...and removed all old submission scripts that hard-coded each experimental design.

---
+++ paper/SyntheticEpitope/job/build_jobs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# FIRST CHANGE PATH TO EXECUTE
+WRK=/path/to/GenoPipe/paper/SyntheticEpitope
+WRK=/Users/olivialang/Documents/GitHub/GenoPipe/paper/SyntheticEpitope
+cd $WRK
+
+TEMPLATE=$WRK/job/depth_template.pbs
+
+N_EXPERIMENTS=`wc -l depth_simulations.txt |awk '{print $1}'`
+
+for (( E=1; E<=$N_EXPERIMENTS; E++ ))
+do
+	INFO=`sed "${E}q;d" depth_simulations.txt`
+	REF=`awk '{print $1}' <(echo $INFO)`
+	PROTEIN=`awk '{print $2}' <(echo $INFO)`
+	LOCUS=`awk '{print $3}'  <(echo $INFO)`
+	EPITOPE=`awk '{print $4}'  <(echo $INFO)`
+	TIME="00:10:00"
+	[[ "$REF" =~ "hg19" ]] && TIME="02:00:00"
+
+	EXPERIMENTNAME=$PROTEIN-$EPITOPE
+	FILENAME=job/run_depth_$E\_$PROTEIN-$LOCUS\_$EPITOPE.pbs
+	echo $FILENAME
+	echo $TEMPLATE
+	sed "s/EXPERIMENTID/${E}/g" $TEMPLATE \
+		| sed "s/EXPERIMENTNAME/${EXPERIMENTNAME}/g" \
+		| sed "s/TIME/${TIME}/g" \
+		> $FILENAME
+done
